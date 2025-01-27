a8e1943710df67e5fb3f28c3faf7bca4e59e6c23
Enzo Soria <esoria.dev@gmail.com>
2025-01-27 14:36:51 -0300

    DB files renamed

--- nuvei-gateway-subscriptions.php
+++ nuvei-gateway-subscriptions.php
@@ -9,14 +9,13 @@
     * Author URI: https://enzosoria.com
     * License: Proprietary
     * License URI: https://enzosoria.com
-    *
     * © 2025 Enzo Soria. Todos los derechos reservados.
     * Este software es propiedad exclusiva de su autor.
     * Su uso, distribución o modificación sin autorización está prohibido.
 **/
 
-require_once plugin_dir_path(__FILE__) . 'includes/admin/plugin-activation.php';
-require_once plugin_dir_path(__FILE__) . 'includes/admin/plugin-description-links.php';
+include plugin_dir_path(__FILE__) . 'includes/admin/plugin-activation.php';
+include plugin_dir_path(__FILE__) . 'includes/admin/plugin-description-links.php';
 include plugin_dir_path(__FILE__) . 'includes/woocommerce/checkout/nuvei-checkout-config.php';
 
 function nuvei_gateway_subscriptions_load_files() {
@@ -29,36 +28,31 @@ function nuvei_gateway_subscriptions_load_files() {
         
 
         if (file_exists($woocommerce_tab_file)) {
-            require_once $woocommerce_tab_file;
+            include $woocommerce_tab_file;
         } else {
             error_log('Error: No se encontró el archivo my-account-tabs.php');
         }
 
         $files_to_include = [
-
             '/auth/create-auth-token.php',
             '/api/debit_with_token.php',
-            '/api/delete_card.php',
+            '/api/delete-user-card.php',
             '/api/list-user-cards.php',
             '/api/refund.php',
-            
-            '/db/create-cards-table.php',
-            '/db/save-user-card.php',
-            '/db/delete-user-card.php',
-            '/db/update_card_from_db.php',
-            '/db/get_card_from_db.php',
-            
+            '/db/create-db-cards-table.php',
+            '/db/save-db-user-card.php',
+            '/db/delete-db-user-card.php',
+            '/db/update-db-user-card.php',
+            '/db/get-db-user-card.php',
             '/includes/hooks/get-current-user-data.php',
             '/includes/hooks/get-plugin-config.php',
             '/includes/hooks/get-nuvei-server-keys.php',
             '/includes/hooks/get-nuvei-app-keys.php',
             '/includes/hooks/get-nuvei-urls.php',
             '/includes/woocommerce/checkout/register-nuvei-gateway.php',
-            
             '/includes/shortcodes/tokenization-form.php',
             '/includes/shortcodes/show-user-cards.php',
             '/includes/shortcodes/refund_form.php',
-            
             '/admin/index.php',
             '/assets/css/index.php',
             
